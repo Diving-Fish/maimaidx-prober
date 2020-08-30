c94581336c9553880e1e21cf0b20e8ca6329813a
Diving-Fish <dfyshisb@163.com>
2020-08-31 02:01:44 +0800

    [add] code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
