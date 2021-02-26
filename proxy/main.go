44c2598295d1ffa5b8be2ad2f9f9859bcbae498c
Diving-Fish <dfyshisb@163.com>
2021-02-26 12:02:10 +0800

    [add] system proxy auto change

--- proxy/main.go
+++ proxy/main.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"golang.org/x/sys/windows/registry"
 	"io"
 	"io/ioutil"
 	"log"
@@ -22,7 +23,21 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
+
+var (
+	ProxyEnable uint64 = 39
+	ProxyServer = "rollback"
+	AutoConfigURL = "rollback"
+)
+
 var jwt *http.Cookie
+
+func commandFatal(prompt string) {
+	rollBack()
+	fmt.Printf("%s请按 Enter 键继续……", prompt)
+	bufio.NewReader(os.Stdin).ReadString('\n')
+	os.Exit(0)
+}
 
 func tryLogin(username string, password string) {
 	body := map[string]interface{}{
@@ -32,9 +47,7 @@ func tryLogin(username string, password string) {
 	b, _ := json.Marshal(&body)
 	resp, _ := http.Post("https://www.diving-fish.com/api/maimaidxprober/login", "application/json", bytes.NewReader(b))
 	if resp.StatusCode != 200 {
-		fmt.Println("登录凭据错误。请按 Enter 键继续……")
-		bufio.NewReader(os.Stdin).ReadString('\n')
-		os.Exit(0)
+		commandFatal("登录凭据错误")
 	}
 	cookies := resp.Cookies()
 	jwt = cookies[0]
@@ -61,13 +74,82 @@ func fetchData(url *url.URL, cookies []*http.Cookie) {
 	}
 	for i := 0; i < 5; i++ {
 		fmt.Printf("正在导入 %s 难度……", labels[i])
-		req, _ := http.NewRequest("GET", "https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=" + strconv.Itoa(i), nil)
+		req, _ := http.NewRequest("GET", "https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff="+strconv.Itoa(i), nil)
 		resp, _ := client.Do(req)
 		commit(resp.Body)
 	}
-	fmt.Println("所有数据均已导入完成，请按 Enter 键以关闭此窗口，不要忘记还原代理设置哦~")
+	if ProxyEnable != 39 {
+		rollBack()
+		fmt.Println("所有数据均已导入完成，请按 Enter 键以关闭此窗口，代理设置已经恢复到先前的设置~")
+	} else {
+		fmt.Println("所有数据均已导入完成，请按 Enter 键以关闭此窗口，不要忘记还原代理设置哦~")
+	}
 	bufio.NewReader(os.Stdin).ReadString('\n')
 	os.Exit(0)
+}
+
+func rollBack() {
+	if ProxyEnable == 39 {
+		return
+	}
+	key, _, _ := registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.ALL_ACCESS)
+	defer key.Close()
+	key.SetDWordValue("ProxyEnable", uint32(ProxyEnable))
+	if ProxyServer != "rollback" {
+		key.SetStringValue("ProxyServer", ProxyServer)
+	}
+	if AutoConfigURL != "rollback" {
+		key.SetStringValue("AutoConfigURL", AutoConfigURL)
+	}
+	_, _ = lib.InternetOptionSettingsChanged()
+}
+
+func setProxyForWin() {
+	_, err := lib.InternetOptionSettingsChanged()
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		return
+	}
+	key, _, _ := registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.ALL_ACCESS)
+	defer key.Close()
+	ProxyEnable, _, err = key.GetIntegerValue("ProxyEnable")
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	err = key.SetDWordValue("ProxyEnable", 1)
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	ProxyServer, _, err = key.GetStringValue("ProxyServer")
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	err = key.SetStringValue("ProxyServer", "127.0.0.1:8033")
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	AutoConfigURL, _, err = key.GetStringValue("AutoConfigURL")
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	err = key.DeleteValue("AutoConfigURL")
+	if err != nil {
+		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
+		rollBack()
+		return
+	}
+	_, _ = lib.InternetOptionSettingsChanged()
+	fmt.Println("代理设置已自动修改。")
 }
 
 func main() {
@@ -76,13 +158,12 @@ func main() {
 		// First run
 		lib.GenerateCert()
 		ioutil.WriteFile("config.json", []byte("{\"username\": \"\", \"password\": \"\"}"), 0644)
-		fmt.Println("初次使用请填写config.json文件，并依据教程完成根证书的安装。请按 Enter 键继续……")
-		bufio.NewReader(os.Stdin).ReadString('\n')
-		os.Exit(0)
+		commandFatal("初次使用请填写config.json文件，并依据教程完成根证书的安装。")
 	}
 	obj := map[string]interface{}{}
 	json.Unmarshal(b, &obj)
 	tryLogin(obj["username"].(string), obj["password"].(string))
+	setProxyForWin()
 	crt, _ := ioutil.ReadFile("cert.crt")
 	pem, _ := ioutil.ReadFile("key.pem")
 	goproxy.GoproxyCa, _ = tls.X509KeyPair(crt, pem)
@@ -95,9 +176,7 @@ func main() {
 			if regexp.MustCompile("^/maimai-mobile/home.*").MatchString(path) {
 				resp.Body = ioutil.NopCloser(strings.NewReader("<p>正在获取您的乐曲数据，请稍候……这可能需要花费数秒，具体进度可以在代理服务器的命令行窗口查看。</p><p>此页面仅用于提示您成功访问了代理服务器，您可以立即关闭此窗口。</p>"))
 				if resp.StatusCode == 302 {
-					fmt.Println("访问舞萌 DX 的成绩界面出错。请按 Enter 键继续……")
-					bufio.NewReader(os.Stdin).ReadString('\n')
-					os.Exit(0)
+					commandFatal("访问舞萌 DX 的成绩界面出错。")
 				}
 				go fetchData(resp.Request.URL, resp.Cookies())
 			}
