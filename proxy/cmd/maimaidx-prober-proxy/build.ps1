00ea86ba3fa58c12846314de135a0cb20c698958
Diving-Fish <dfyshisb@163.com>
2021-04-03 00:58:44 +0800

    [feature] cross-platform compile

---
+++ proxy/cmd/maimaidx-prober-proxy/build.ps1
@@ -0,0 +1,16 @@
+$version = "1.1.0"
+$filename = "maimaidx-prober-proxy-windows-$version.exe"
+go env -w CGO_ENABLED=1
+go env -w GOOS=windows
+go env -w GOARCH=amd64
+go build -o $filename
+$filename = "maimaidx-prober-proxy-mac-$version"
+go env -w CGO_ENABLED=0
+go env -w GOOS=darwin
+go env -w GOARCH=amd64
+go build -o $filename
+$filename = "maimaidx-prober-proxy-linux-$version"
+go env -w CGO_ENABLED=0
+go env -w GOOS=linux
+go env -w GOARCH=amd64
+go build -o $filename
