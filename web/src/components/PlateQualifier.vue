b222a56355ad26950c59fa77fd8f911dae2256d8
Diving-Fish <dfyshisb@163.com>
2021-09-06 17:09:31 +0800

    [update]调整了AP和FSDX的顺序

--- web/src/components/PlateQualifier.vue
+++ web/src/components/PlateQualifier.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog
     v-model="visible"
-    :width="masOnly ? 500 : 800"
+    :width="masOnly ? 500 : 1000"
     :fullscreen="$vuetify.breakpoint.mobile"
   >
     <template #activator="{ on, attrs }">
@@ -28,14 +28,14 @@
         <div>
           <v-icon color="green">mdi-check-bold</v-icon>=Full Combo
           <v-icon color="blue">mdi-check-bold</v-icon>=SSS
-          <v-icon color="orange">mdi-check-bold</v-icon>=All Perfect
           <v-icon color="purple">mdi-check-bold</v-icon>=Full Sync DX
+          <v-icon color="orange">mdi-check-bold</v-icon>=All Perfect
         </div>
         <div class="mt-2">
           <v-icon color="green">mdi-check-bold</v-icon>{{total_1}}/{{filted.length * (masOnly ? 1 : 4)}}
           <v-icon color="blue">mdi-check-bold</v-icon>{{total_2}}/{{filted.length * (masOnly ? 1 : 4)}}
-          <v-icon color="orange">mdi-check-bold</v-icon>{{total_4}}/{{filted.length * (masOnly ? 1 : 4)}}
-          <v-icon color="purple">mdi-check-bold</v-icon>{{total_8}}/{{filted.length * (masOnly ? 1 : 4)}}
+          <v-icon color="purple">mdi-check-bold</v-icon>{{total_4}}/{{filted.length * (masOnly ? 1 : 4)}}
+          <v-icon color="orange">mdi-check-bold</v-icon>{{total_8}}/{{filted.length * (masOnly ? 1 : 4)}}
         </div>
         <v-data-table
           :headers="masOnly ? headerMas : headers"
@@ -47,40 +47,40 @@
           <template #item.bas_pq="{ item }">
             <v-icon color="green" v-if="item.bas_pq & 1">mdi-check-bold</v-icon>
             <v-icon color="blue" v-if="item.bas_pq & 2">mdi-check-bold</v-icon>
-            <v-icon color="orange" v-if="item.bas_pq & 4"
+            <v-icon color="purple" v-if="item.bas_pq & 4"
               >mdi-check-bold</v-icon
             >
-            <v-icon color="purple" v-if="item.bas_pq & 8"
+            <v-icon color="orange" v-if="item.bas_pq & 8"
               >mdi-check-bold</v-icon
             >
           </template>
           <template #item.adv_pq="{ item }">
             <v-icon color="green" v-if="item.adv_pq & 1">mdi-check-bold</v-icon>
             <v-icon color="blue" v-if="item.adv_pq & 2">mdi-check-bold</v-icon>
-            <v-icon color="orange" v-if="item.adv_pq & 4"
+            <v-icon color="purple" v-if="item.adv_pq & 4"
               >mdi-check-bold</v-icon
             >
-            <v-icon color="purple" v-if="item.adv_pq & 8"
+            <v-icon color="orange" v-if="item.adv_pq & 8"
               >mdi-check-bold</v-icon
             >
           </template>
           <template #item.exp_pq="{ item }">
             <v-icon color="green" v-if="item.exp_pq & 1">mdi-check-bold</v-icon>
             <v-icon color="blue" v-if="item.exp_pq & 2">mdi-check-bold</v-icon>
-            <v-icon color="orange" v-if="item.exp_pq & 4"
+            <v-icon color="purple" v-if="item.exp_pq & 4"
               >mdi-check-bold</v-icon
             >
-            <v-icon color="purple" v-if="item.exp_pq & 8"
+            <v-icon color="orange" v-if="item.exp_pq & 8"
               >mdi-check-bold</v-icon
             >
           </template>
           <template #item.mst_pq="{ item }">
             <v-icon color="green" v-if="item.mst_pq & 1">mdi-check-bold</v-icon>
             <v-icon color="blue" v-if="item.mst_pq & 2">mdi-check-bold</v-icon>
-            <v-icon color="orange" v-if="item.mst_pq & 4"
+            <v-icon color="purple" v-if="item.mst_pq & 4"
               >mdi-check-bold</v-icon
             >
-            <v-icon color="purple" v-if="item.mst_pq & 8"
+            <v-icon color="orange" v-if="item.mst_pq & 8"
               >mdi-check-bold</v-icon
             >
           </template>
@@ -132,8 +132,8 @@ export default {
       let r = this.records_filter(title, type, diff);
       if (r.length == 0) return 0;
       let a = 0;
-      if (["fsd", "fsdp"].indexOf(r[0].fs) != -1) a += 8;
-      if (["ap", "app"].indexOf(r[0].fc) != -1) a += 4;
+      if (["fsd", "fsdp"].indexOf(r[0].fs) != -1) a += 4;
+      if (["ap", "app"].indexOf(r[0].fc) != -1) a += 8;
       if (r[0].achievements >= 100) a += 2;
       if (["fc", "fcp", "ap", "app"].indexOf(r[0].fc) != -1) a += 1;
       // console.log(a);
