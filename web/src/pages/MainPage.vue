39fb717ef8c09d096a3eac228f7447cecdac2020
Diving-Fish <dfyshisb@163.com>
2022-09-26 10:14:25 +0800

    Add afdian

--- web/src/pages/MainPage.vue
+++ web/src/pages/MainPage.vue
@@ -22,6 +22,7 @@
       <p class="mt-3">欢迎加入舞萌DX查分器交流群：476936821（2群） 981682758（已满）</p>
       <p>代理工具上线！使用微信客户端导入数据，请查看新版本的使用指南。</p>
       <p>想要 10 分钟搭建自己的 maimai QQ 机器人？现在就参考开源项目 <a href="https://github.com/Diving-Fish/mai-bot">mai-bot</a> 吧~</p>
+      <p>请开发者打一局 maimai 如何？帮助我们<a href="https://afdian.net/a/divingfish">发发电</a>好不好嘛~</p>
       <p style="color: #f44336">
         迁移了数据库以加快网站的响应速度及后续开发。如遇任何无法导入成绩或出错的情况，请及时添加讨论群进行反馈。
       </p>
