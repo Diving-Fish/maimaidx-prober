632c076f87caa743a37a9e23e4a686dcf4c2bddf
WANG Xuerui <git@xen0n.name>
2023-05-29 20:29:49 +0800

    [refactor] gather system proxy setting logic and state into own manager type
    And clean up things while at it.

--- proxy/cmd/maimaidx-prober-proxy/system_proxy_mgr_windows.go
+++ proxy/cmd/maimaidx-prober-proxy/system_proxy_mgr_windows.go
@@ -11,23 +11,37 @@ import (
 	"github.com/Diving-Fish/maimaidx-prober/proxy/lib"
 )
 
-func rollbackSystemProxySettings() {
-	if ProxyEnable == 39 {
+type systemProxyManager struct {
+	ProxyEnable   uint64
+	ProxyServer   string
+	AutoConfigURL string
+}
+
+func newSystemProxyManager(addr string) *systemProxyManager {
+	return &systemProxyManager{
+		ProxyEnable:   39,
+		ProxyServer:   "rollback",
+		AutoConfigURL: "rollback",
+	}
+}
+
+func (s *systemProxyManager) rollback() {
+	if s.ProxyEnable == 39 {
 		return
 	}
 	key, _, _ := registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.ALL_ACCESS)
 	defer key.Close()
-	key.SetDWordValue("ProxyEnable", uint32(ProxyEnable))
-	if ProxyServer != "rollback" {
-		key.SetStringValue("ProxyServer", ProxyServer)
+	key.SetDWordValue("ProxyEnable", uint32(s.ProxyEnable))
+	if s.ProxyServer != "rollback" {
+		key.SetStringValue("ProxyServer", s.ProxyServer)
 	}
-	if AutoConfigURL != "rollback" {
-		key.SetStringValue("AutoConfigURL", AutoConfigURL)
+	if s.AutoConfigURL != "rollback" {
+		key.SetStringValue("AutoConfigURL", s.AutoConfigURL)
 	}
 	_, _ = lib.InternetOptionSettingsChanged()
 }
 
-func applySystemProxySettings() {
+func (s *systemProxyManager) apply() {
 	_, err := lib.InternetOptionSettingsChanged()
 	if err != nil {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
@@ -35,44 +49,44 @@ func applySystemProxySettings() {
 	}
 	key, _, _ := registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.ALL_ACCESS)
 	defer key.Close()
-	ProxyEnable, _, err = key.GetIntegerValue("ProxyEnable")
+	s.ProxyEnable, _, err = key.GetIntegerValue("ProxyEnable")
 	if err != nil {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-		rollbackSystemProxySettings()
+		s.rollback()
 		return
 	}
 	err = key.SetDWordValue("ProxyEnable", 1)
 	if err != nil {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-		rollbackSystemProxySettings()
+		s.rollback()
 		return
 	}
-	ProxyServer, _, err = key.GetStringValue("ProxyServer")
+	s.ProxyServer, _, err = key.GetStringValue("ProxyServer")
 	if err != nil {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-		rollbackSystemProxySettings()
+		s.rollback()
 		return
 	}
 	err = key.SetStringValue("ProxyServer", "127.0.0.1:8033")
 	if err != nil {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-		rollbackSystemProxySettings()
+		s.rollback()
 		return
 	}
-	AutoConfigURL, _, err = key.GetStringValue("AutoConfigURL")
+	s.AutoConfigURL, _, err = key.GetStringValue("AutoConfigURL")
 	if err != nil {
 		if err == syscall.ENOENT {
-			AutoConfigURL = "rollback"
+			s.AutoConfigURL = "rollback"
 		} else {
 			fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-			rollbackSystemProxySettings()
+			s.rollback()
 			return
 		}
 	}
 	err = key.DeleteValue("AutoConfigURL")
 	if err != nil && err != syscall.ENOENT {
 		fmt.Println("自动修改代理设置失败。请尝试手动修改代理。")
-		rollbackSystemProxySettings()
+		s.rollback()
 		return
 	}
 	_, _ = lib.InternetOptionSettingsChanged()
