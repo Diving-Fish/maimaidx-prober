ddf5d034c4d7aff487f1b3db617bc58a5fd0dae7
Diving-Fish <dfyshisb@163.com>
2024-08-08 16:36:29 +0800

    [Update] Vue 2 to latest version

---
+++ web/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        'vue/multi-word-component-names': 'warn',
+        'vue/valid-v-slot': 'warn',
+        'vue/no-mutating-props': 'warn',
+    },
+}
--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
