55e3774b878c4cc0f30cf96dc65469a145362a69
Typed SIGTERM <typed.sigterm@gmail.com>
2024-09-16 23:14:34 +0800

    [fix] afdian link

--- web/src/pages/MainPage.vue
+++ web/src/pages/MainPage.vue
@@ -21,7 +21,7 @@
       <p class="mt-3">欢迎加入查分器<a href="https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&appChannel=share&biz=ka&businessType=5&from=181075&inviteCode=20DoWXWylop&mainSourceId=qr_code&subSourceId=pic4&jumpsource=shorturl#/out">QQ频道</a>！</p>
       <p>代理工具上线！使用微信客户端导入数据，请查看新版本的使用指南。</p>
       <p>想要 10 分钟搭建自己的 maimai QQ 机器人？现在就参考开源项目 <a href="https://github.com/Diving-Fish/mai-bot">mai-bot</a> 吧~</p>
-      <p>请开发者打一局 maimai 如何？帮助我们<a href="https://afdian.net/a/divingfish">发发电</a>好不好嘛~</p>
+      <p>请开发者打一局 maimai 如何？帮助我们<a href="https://afdian.com/a/divingfish">发发电</a>好不好嘛~</p>
       <p style="color: #f44336">
         迁移了数据库以加快网站的响应速度及后续开发。如遇任何无法导入成绩或出错的情况，请及时添加讨论群进行反馈。
       </p>
